46470ce9c25b6e644f03b6f3c805006a79abfa53
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-04-19 19:27:07 -0300

    create setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_lambda',
+    version='0.1',
+    packages=['src', 'src.errors', 'src.example', 'src.helpers'],
+    url='https://github.com/Maua-Dev/python_lambda',
+    license='',
+    author='Bruno Vilardi',
+    author_email='brunovilardibueno@gmail.com',
+    description='Microframework to develop python APIs on Lambda'
+)
