268f72dc2598ca1df8399c8f7803cb8315a81cc3
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-04-19 21:59:01 -0300

    convert only the body to json before using it

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_lambda',
-    version='0.6',
+    version='0.7',
     packages=['python_lambda', 'python_lambda.src', 'python_lambda.src.errors', 'python_lambda.src.helpers'],
     url='https://github.com/Maua-Dev/python_lambda',
     license='',
